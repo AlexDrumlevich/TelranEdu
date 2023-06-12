51fe3dcacf9c91c8dd16f7251e002fd86ddd9f7f
Alexey Drumlevich <l-gc@yandex.ru>
2023-06-12 18:02:04 +0300

    Streams int

--- TelranEdu/src/telran/util/stream/PrimitiveStreams.java
+++ TelranEdu/src/telran/util/stream/PrimitiveStreams.java
@@ -1,9 +1,15 @@
 package telran.util.stream;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.PrimitiveIterator.OfInt;
 import java.util.Random;
+import java.util.stream.IntStream;
+
+import telran.util.HashSet;
 
 public class PrimitiveStreams {
-	
+
 	static public int[] randomUnique(int nNumbers, int minInclusive,
 			int maxExclusive) {
 		if (maxExclusive - minInclusive < nNumbers) {
@@ -13,4 +19,41 @@ public class PrimitiveStreams {
 				.distinct().limit(nNumbers).toArray();
 	}
 
+	static public int[][] twoDimensionalRandomArray(int countRows, int countColumns, int minInclusive,
+			int maxExclusive) {
+		int[][] array = new int[countRows][countColumns];
+		return Arrays
+				.stream(array)
+				.map(n -> randomUnique(countColumns, minInclusive, maxExclusive))
+				.toArray(int[][]::new);
+	}
+
+	
+	static public int[] getSuffledArray(int[] array) {
+		
+		//Set to keep and get distinct indexes
+		HashSet<Integer> setOfIndexes = new HashSet<>();
+		for(int i = 0; i < array.length; i ++) {
+			setOfIndexes.add(i);
+		}
+		//to get random position of setOfIndexes iterator 
+		Random randomIndex = new Random();
+
+		return Arrays
+				.stream(array)
+				.map(n -> {
+			//get and remove random position in setOfIndexes
+			Iterator<Integer> setOfIndexesIterator = setOfIndexes.iterator();
+			int i = 0;
+			while(i < randomIndex.nextInt(setOfIndexes.size())) {
+				setOfIndexesIterator.next();
+				i ++;
+			}
+			int calculatedRandomIndex = setOfIndexesIterator.next();
+			setOfIndexesIterator.remove();
+			//change current value to value from array by random index 
+			return array[calculatedRandomIndex];
+		})
+				.toArray(); 
+	}
 }
--- TelranEdu/src/telran/util/stream/test/PrimitiveStreamTest.java
+++ TelranEdu/src/telran/util/stream/test/PrimitiveStreamTest.java
@@ -2,6 +2,7 @@ package telran.util.stream.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import telran.util.HashSet;
@@ -9,6 +10,8 @@ import telran.util.HashSet;
 import static telran.util.stream.PrimitiveStreams.*;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
 
 class PrimitiveStreamTest {
 
@@ -39,7 +42,6 @@ class PrimitiveStreamTest {
 			}
 		}
 		assertTrue(index < arrayPrev.length);
-
 	}
 
 	private void runUniqueTest(int[] array) {
@@ -48,7 +50,44 @@ class PrimitiveStreamTest {
 			set.add(num);
 		}
 		assertEquals(array.length, set.size());
-
 	}
+
+
+
+	@Test
+	void shuffleTest() {
+		for (int i = 0; i < N_RUNS; i++) {
+			int[] arrayPrevious = new Random().ints().limit(N_RUNS).toArray();
+			int[] arraySuffled1 = getSuffledArray(Arrays.copyOf(arrayPrevious, arrayPrevious.length));
+			int[] arraySuffled2 = getSuffledArray(Arrays.copyOf(arrayPrevious, arrayPrevious.length));
+
+			// Equals length
+			assertEquals(arrayPrevious.length, arraySuffled1.length);
+			assertEquals(arrayPrevious.length, arraySuffled2.length);
+
+			//arrays not equal
+			assertFalse(Arrays.equals(arrayPrevious, arraySuffled1));
+			assertFalse(Arrays.equals(arrayPrevious, arraySuffled2));
+			assertFalse(Arrays.equals(arraySuffled1, arraySuffled2));
+
+			//arrays equal	
+			assertArrayEquals(Arrays.stream(arrayPrevious).sorted().toArray(), Arrays.stream(arraySuffled1).sorted().toArray());
+			assertArrayEquals(Arrays.stream(arrayPrevious).sorted().toArray(), Arrays.stream(arraySuffled2).sorted().toArray());
+
+		}
+	}
+
+	@Test
+	void twoDimensionalRandomArrayTest() {
+		int[][] array = twoDimensionalRandomArray(4, 6, MIN_NUMBER, MAX_NUMBER);
+		for(int i = 0; i < array.length; i ++) {
+			for(int j = 0; j < array[i].length; j ++) {
+				System.out.print(" " + array[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+
 
 }
