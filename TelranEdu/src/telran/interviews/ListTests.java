3b4443363dbc3fd1af7cfefed8babe6361846fd1
Alexey Drumlevich <l-gc@yandex.ru>
2023-07-13 10:08:35 +0300

    standart collection initial

---
+++ TelranEdu/src/telran/interviews/ListTests.java
@@ -0,0 +1,97 @@
+package telran.interviews;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+class ListTests {
+Integer[] numbers = {10, -30, 13, 48, 22,17, 23};
+List<Integer> immutableList = Arrays.asList(numbers);
+List<Integer> mutableList;
+	@BeforeEach
+	void setUp() throws Exception {
+		mutableList = new ArrayList<>(immutableList);
+	}
+
+	@Test
+	void toArrayTest() {
+		assertIterableEquals(mutableList, immutableList);
+		assertArrayEquals(numbers, mutableList.toArray(Integer[]::new));
+		assertArrayEquals(numbers, immutableList.toArray(Integer[]::new));
+	}
+	@Test
+	void updateListsTest() {
+		assertThrowsExactly(UnsupportedOperationException.class,
+				() -> immutableList.remove(0));
+		List<Integer> subList = mutableList.subList(2, 5);
+		subList.clear();
+		Integer[] expected1 = {10, -30, 17, 23};
+		assertArrayEquals(expected1, mutableList.toArray(Integer[]::new));
+		subList = mutableList.subList(2, 4);
+		subList.set(1, 500);
+		Integer[] expected2 = {10, -30, 17, 500};
+		assertArrayEquals(expected2, mutableList.toArray(Integer[]::new));
+		subList.add(0, -50);
+		Integer[] expected3 = {10, -30, -50, 17, 500};
+		assertArrayEquals(expected3, mutableList.toArray(Integer[]::new));
+		Integer[] expected4 = {-50, 17, 500};
+		Integer[] expected5 = {10, -30, -50, 17};
+		subList.remove(2);
+//		List<Integer> subList2 = mutableList.subList(0, 2);
+//		subList.add(40);
+//		//assertArrayEquals(expected5, mutableList.toArray(Integer[]::new));
+//		System.out.println(subList2);
+		
+		
+		
+		
+		
+		
+	}
+	@Test
+	void stackTest() {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(10);
+		assertFalse(stack.isEmpty());
+		assertEquals(10, stack.pop());
+		assertTrue(stack.isEmpty());
+		
+		
+	}
+	@Test
+	void queueTest() {
+		Queue<Integer> queue = new LinkedList<>(); 
+		assertThrowsExactly(NoSuchElementException.class, ()->queue.remove());
+	}
+
+	
+	@Test
+	void stackIntTest() {
+		StackInt stackInt = new StackInt();
+		assertThrowsExactly(NoSuchElementException.class, () -> stackInt.pop());
+		assertThrowsExactly(NoSuchElementException.class, () -> stackInt.max());
+		assertTrue(stackInt.isEmpty());
+		
+		stackInt.push(30);
+		assertFalse(stackInt.isEmpty());
+		stackInt.push(100);
+		stackInt.push(-100);
+		stackInt.push(50);
+		assertEquals(100, stackInt.max());
+		assertEquals(50, stackInt.pop());
+		assertEquals(100, stackInt.max());
+		assertEquals(-100, stackInt.pop());
+		assertEquals(100, stackInt.max());
+		assertEquals(100, stackInt.pop());
+		assertEquals(30, stackInt.max());
+		assertEquals(30, stackInt.pop());
+		
+		assertThrowsExactly(NoSuchElementException.class, () -> stackInt.pop());
+		assertThrowsExactly(NoSuchElementException.class, () -> stackInt.max());
+		assertTrue(stackInt.isEmpty());
+			
+	}
+	
+}
---
+++ TelranEdu/src/telran/interviews/StackInt.java
@@ -0,0 +1,76 @@
+package telran.interviews;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+/**
+ * 
+ * All methods have to have the Complexity O[1]
+ *
+ */
+//max value
+//StackNode uses LinkedList of StackNode class objects
+//each node has max value
+//if max value of head StackNode is more then new value => this max values 
+//is used as max value for new StackNode,
+//if not, new int value is set as value for new StackNode, but for second node  old max value doesn`t change
+public class StackInt {
+	//TODO fields
+	/**
+	 * 
+	 * @param num
+	 * adds num in the stack
+	 */
+	LinkedList<StackNode> linkedList = new LinkedList<>();
+
+	static private class StackNode {
+		int value;
+		int localMax;
+
+		public StackNode(int value, int localMax) {
+			this.value = value;
+			this.localMax = localMax;
+		}
+	}
+
+
+	public void push(int num) {
+		//TODO
+
+		linkedList.addFirst(new StackNode(num, (!isEmpty() && max() > num) ? max() : num));
+	}
+
+	/**
+	 * 
+	 * @return
+	 * takes out and returns the top stack number
+	 * in the case the stack is empty the exception of the class NoSuchElement should be thrown
+	 */
+	public int pop() {
+		if(isEmpty()) {
+			throw new NoSuchElementException("stack is empty");
+		}
+		return linkedList.removeFirst().value;
+	}
+
+	/**
+	 * 
+	 * @return true if the stack is empty
+	 */
+	public boolean isEmpty() {
+		//TODO
+		return linkedList.isEmpty();
+	}
+	/**
+	 * 
+	 * @return maximal value existing in the stack
+	 * throws NoSuchElementException in the case the stack is empty
+	 */
+	public int max() {
+		//TODO
+		if(isEmpty()) {
+			throw new NoSuchElementException("stack is empty");
+		}
+		return linkedList.getFirst().localMax;
+	}
+}
