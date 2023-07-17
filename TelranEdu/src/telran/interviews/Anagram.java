691c41082eb2b6329f7be50d1a5cabe3702dcb7d
Alexey Drumlevich <l-gc@yandex.ru>
2023-07-17 20:53:30 +0300

    fix

--- TelranEdu/src/telran/interviews/Anagram.java
+++ TelranEdu/src/telran/interviews/Anagram.java
@@ -16,10 +16,20 @@ public class Anagram {
 	public static boolean isAnagram(String word, String anagram) {
 		//TODO
 		boolean result = true;
+		//mapWord - contains chars int of word
+		//key - int char
+		//value - amount of this char in the word  
 		HashMap<Integer, Integer> mapWord = new HashMap<>();
+		//get old value and add new value 
 		word.chars().forEach(i -> mapWord.merge(i, 1, (a, b) -> a + b));
 		
+		//for each by chars of anagram
+		//get value from mapWord by key, and reduce value by 1, 
+		//if value already 1 => remove 
+		//if there is not key => word does not contain char from anagram 
+		//(or contains in smaller quantities)
 		for(int charInt : anagram.chars().toArray()) {
+			
 			Integer value = mapWord.get(charInt);
 			if(value == null) {
 				result = false;
@@ -30,7 +40,8 @@ public class Anagram {
 				mapWord.merge(charInt, 1, (a, b) -> a - b);
 			}
 		}
-		
+		//if there is at least one value in mapWord after for each by chars of anagram
+		//=> word contains more chars then anagram
 		return result ? mapWord.isEmpty() : false;
 		
 	}
